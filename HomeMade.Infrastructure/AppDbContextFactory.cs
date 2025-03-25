e99428d74d9ece10613b500f3eef846f35964f49
Omar Selim <ommarselim@gmail.com>
2025-03-25 02:46:25 +0200

    Add DbContextFactory And Seed Default Data

---
+++ HomeMade.Infrastructure/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using HomeMade.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeMade.Infrastructure
+{
+    class AppDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=OMAR\\SQL2019;Database=HomeMadeDB;Trusted_Connection=True;TrustServerCertificate=True;");
+            return new ApplicationDbContext(optionsBuilder.Options);
+
+        }
+    }
+}
